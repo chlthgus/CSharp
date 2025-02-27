f3cc29d3e76fcd4563731091188c441de8dfc9ba
chlthgus <fps8295@naver.com>
2025-02-27 18:44:21 +0900

    1. 프로젝트 셋팅
    1. 프로젝트 셋팅

---
+++ Assets/Script/HelloWorld.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello Unity!!!");
+        Debug.Log("123456789");
+
+        Debug.Log("Hello Unity!!!\\n123456789");
+    }
+
+    /*
+     [output] : 콘솔창에 아래  내용을 출력하세요
+     Hello Unity!!!
+     123456789
+     */
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Variable/VariablePeatice.cs
@@ -0,0 +1,21 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class VariablePeatice : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int First = 10;
+        Debug.Log("First : " + First);
+
+    }
+}
+
+/*
+[Q]
+변수: first, second, thied 정의하고 값 대입하여 아래내용 출력
+
+[out put]
+10,20,30
+*/
